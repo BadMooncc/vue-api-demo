9361576e2768c8b28232781d4a051031d6b3ea8d
zhaodenghui <zhaodenghui@iyunxiao.com>
2018-08-07 18:09:49 +0800

    midify style

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <h1>vue常用API示例代码</h1>
+    <filter-demo></filter-demo>
     <router-view/>
   </div>
 </template>
@@ -12,12 +13,7 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+ h1,h2,p{
+   text-align: center;
+ }
 </style>
